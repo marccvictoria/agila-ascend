29b365470f41fd821089ea7ae9f25244712cd4dc
marccvictoria <marctristanvictoria@gmail.com>
2025-07-29 19:55:57 +0800

    added game over menu and updated games assets

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ let gameWidth = 800;
 let ctx; // context refers to the rendering context =  provides the drawing functions & properties
 
 // birdie
-let birdWidth = 34; // in pixels
-let birdHeight = 24; // w/h ratio = 17/12
+let birdWidth = 34 * 1.5; // in pixels
+let birdHeight = 24 * 1.5; // scale up by %
 // init pos of birdie
 let birdX = gameWidth/8;
 let birdY = gameHeight/2;
@@ -41,6 +41,24 @@ let gravity = 0.4;
 let isGameOver = false;
 let score = 0;
 
+// game over pop up
+let gameOverScore;
+let restart;
+
+let GOSProp = {
+    x: gameWidth/3.2,
+    y: gameHeight/8,
+    width: 168 * 1.8,  // 168 x 224
+    height: 224 * 1.8
+}
+
+let resProp = {
+    x: gameWidth/2.8,
+    y: (gameHeight * 3)/4,
+    width: 561 * 0.4, // 209 x 561 (actual image)
+    height: 209 * 0.4
+}
+
 // When the entire page finishes loading, run this function
 // browser will automatically invoke them when that event happens
 window.onload = function() {
@@ -52,7 +70,7 @@ window.onload = function() {
 
     // Load Images
     birdImg = new Image(); // Create an HTMLImage Element
-    birdImg.src = "./assets/flappybird.gif";
+    birdImg.src = "./assets/eagle.png";
     // birdImg variable hasn't loaded yet, so we load it
     birdImg.onload = function() {
         ctx.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
@@ -64,9 +82,16 @@ window.onload = function() {
     bottomPipeImg = new Image();
     bottomPipeImg.src = "./assets/bottompipe.png"
 
+    gameOverScore = new Image();
+    gameOverScore.src = "./assets/game_menu/score_best.png"
+
+    restart = new Image();
+    restart.src = "./assets/game_menu/restart.png"
+
     document.addEventListener('keyup', (e) => {
     if (e.code === 'Space' || e.code === 'ArrowUp'){
         birdVeloY = -6; // this is the upward jump, the lower the val, the higher the jump
+        playAudio("wing");
 
         // reset game if game over
         if (isGameOver) {
@@ -115,8 +140,8 @@ function update() {
 
     // score
     ctx.fillStyle = "white";
-    ctx.font = "45px sans-serif";
-    ctx.fillText(score, 5, 45) // var, x, y pos
+    ctx.font = '32px "Press Start 2P"';
+    ctx.fillText("Score: " + score, 5, 45) // var, x, y pos
 
     // clear pipes to free up memory
     while (pipeArr.length > 0 && pipeArr[0].x < -pipeWidth) { // instead of 0, -pipeWidth is used = right side of pipe
@@ -134,7 +159,17 @@ function update() {
     }
     
     if (isGameOver){
-        ctx.fillText("Game Over", 5, 90);
+        ctx.drawImage(gameOverScore, GOSProp.x, GOSProp.y, GOSProp.width, GOSProp.height);
+        ctx.drawImage(restart, resProp.x, resProp.y, resProp.width, resProp.height);
+        ctx.font = '50px "Press Start 2P"';
+        ctx.fillStyle = '#90EE90';
+        ctx.strokeStyle = '#013220';
+        ctx.lineWidth = 2;
+        ctx.fillText("Game Over", gameWidth/4.5, 90);
+        ctx.strokeText("Game Over", gameWidth/4.5, 90);
+        ctx.font = '48px "Press Start 2P"';
+        ctx.fillText(score, GOSProp.width/0.8, GOSProp.height/1.7);
+        ctx.strokeText(score, GOSProp.width/0.8, GOSProp.height/1.7);
     }
 }
 
@@ -178,10 +213,18 @@ function detectCollision(a, b) { // rectangles
 
 function playAudio(status) {
     if (status == "point") {
-        var audio = new Audio('./assets/sfx_point.wav');
+        var audio = new Audio('./assets/music/sfx_point.wav');
         audio.play();
     } else if (status == "die") {
-        var audio = new Audio('./assets/sfx_die.wav');
+        var audio = new Audio('./assets/music/sfx_die.wav');
+        audio.play();
+    } else if (status == "wing") {
+        var audio = new Audio('./assets/music/sfx_swooshing.wav');
         audio.play();
     }
+}
+
+function birdRotUp() {
+    ctx.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    ctx.rotate((45 * Math.PI) / 180)
 }
